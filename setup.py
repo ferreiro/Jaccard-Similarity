2b57375a775d3fa7775b9f3613f3c8a4e0388846
Jorge Ferreiro <me@jgferreiro.com>
2016-03-06 00:01:19 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Command line application to recommend product
+    based on Jaccard similarity
+"""
+from setuptools import setup
+# import os
+# import sys
+
+
+setup(
+    name='Jaccard Similarity',
+    version='1.0',
+    url='https://github.com/ferreiro/Jaccard-Similarity',
+    author='Jorge Ferreiro',
+    author_email='me@jgferreiro.com',
+    description=('Command line application to recommend products',
+                 'based on Jaccard similarity'),
+    license='MIT',
+    packages=[]
+)
