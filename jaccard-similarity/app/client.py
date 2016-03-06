ef661d06c4e7f5b315c13151cadfd93147deebe7
Jorge Ferreiro <me@jgferreiro.com>
2016-03-06 04:03:55 +0100

    Refactored methods + random purchases generation

--- jaccard-similarity/app/client.py
+++ jaccard-similarity/app/client.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from random import randint
+
 N_CATEGORIES = 5
 BASE_PROBABILITY = 10
 TOP_PROBABILITY = 30
+MAX_BOUGHT_PRODUCTS = 10
 
-# Constant factor used for calculating
-# the total number of purchased one client has done
-CLIENT_TYPE = {
+# Constant Dictionary where the value (right element)
+# is the factor by we multiply the number of random purchases
+# make by the user when calculating the number of purchases
+CLIENT_RECURRENCY_FACTOR = {
     'low': 1,
     'medium': 2,
     'high': 3
@@ -15,22 +19,42 @@ CLIENT_TYPE = {
 
 class Client(object):
 
-    def __init__(self, name='', _type='low'):
-        self.name = 2
-        self.products = 0
-        self._type = _type
+    def __init__(self, name='', recurrency='low', fav_category=''):
+        self.name = name
+        self.purchases_number = 0  # Number of sold products
+        self.fav_category = fav_category
+        self.recurrency = recurrency
+
+    # GETTERS
+    def get_purchases_number(self):
+        return self.purchases_number
 
     # METHODS
-    def buy_products(self, client_top_cagegory):
+    def get_recurrency_num(self, key, recurrency_dict):
+        if key in recurrency_dict:
+            return recurrency_dict[key]
+        else:
+            return 0
+
+    def set_random_purchases_number(self, recurrency_key):
+        random_purchases_num = randint(1, MAX_BOUGHT_PRODUCTS-1)
+        increase_purchases_by_factor = self.get_recurrency_num(
+            recurrency_key, CLIENT_RECURRENCY_FACTOR)
+
+        total_purchases = random_purchases_num * increase_purchases_by_factor
+
+        self.purchases_number = total_purchases
+
+    def buy_products(self, client_fav_category):
         """
         Return a list containing probabilities
         that the client buy products from each category.
-        When 'client_top_cagegory' is provided, we
+        When 'fav_category' is provided, we
         assign 3x more probabilities to that category
         """
         total = BASE_PROBABILITY * N_CATEGORIES
         products = [0] * N_CATEGORIES
-        top_cat_index = client_top_cagegory
+        top_cat_index = client_fav_category
 
         for i in range(0, N_CATEGORIES):
             products[i] = BASE_PROBABILITY
@@ -47,8 +71,8 @@ class Client(object):
 
         return products
 
-    def get_purchased_volume_num(self, key, purchased_volume_dict):
-        if key in purchased_volume_dict:
-            return purchased_volume_dict[key]
-        else:
-            return 0
+    def generate_random_information(self, recurrency, fav_category):
+        pass
+
+    # def is_valid_recurrency(self, recurrency, valid_recurrencies_dict):
+    #     return recurrency in valid_recurrencies_dict
--- jaccard-similarity/test/test_client.py
+++ jaccard-similarity/test/test_client.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 from app.client import Client
 
+
 class Client_tests(unittest.TestCase):
 
     def setUp(self):
@@ -25,29 +26,52 @@ class Client_tests(unittest.TestCase):
             products_max = max(products)
             self.assertEqual(products_min, products_max)
 
-    def test_client_purchased_volume_converts_str_to_int(self):
-        purchased_volume = {
+    def test_client_recurrency_converts_str_to_int(self):
+        recurrency = {
             'low': 1,
             'medium': 2,
             'high': 3
         }
         key = 'high'
-        v = self.c.get_purchased_volume_num(key, purchased_volume)
+        v = self.c.get_recurrency_num(key, recurrency)
         self.assertEqual(v, 3)
 
-    def test_client_purchased_volume_returns_cero_key_not_found_or_valid(self):
-        purchased_volume = {
+    def test_client_recurrency_returns_cero_when_key_not_found_or_valid(self):
+        recurrency = {
             'low': 1
         }
         not_valid_keys = (None, 3300030303033030, 'yes')
         for key in not_valid_keys:
-            vol_num = self.c.get_purchased_volume_num(key, purchased_volume)
+            vol_num = self.c.get_recurrency_num(key, recurrency)
             self.assertEqual(vol_num, 0)
+    #
+    # def test_client_returns_false_when_not_valid_recurrency_name(self):
+    #     wrong_recurrencies = ('fdsfsdfd', 'highhigh', 34343, None)
+    #     valid_recurrency_dict = {
+    #         'low': 1,
+    #         'medium': 2,
+    #         'high': 3
+    #     }
+    #
+    #     for value in wrong_recurrencies:
+    #         valid = self.c.is_valid_recurrency(value, valid_recurrency_dict)
+    #         self.assertEqual(valid, False)
 
-    def test_client_generate_correct_number_of_purchases(self):
-        c_type = 'low'
-        c = Client(c_type)
+    def test_client_number_of_products_updated_when_valid_recurrency(self):
+        recurrency_key = 'high'
+        previous_purchases_number = self.c.get_purchases_number()
+        self.c.set_random_purchases_number(recurrency_key)
+        after_purchases_number = self.c.get_purchases_number()
 
+        self.assertNotEqual(previous_purchases_number, after_purchases_number)
+
+    def test_client_number_of_products_are_0_when_wrong_recurrency(self):
+        recurrency_key = 'sdfsdlfjhsdkjghdfkgjhdfskjghds'
+        previous_purchases_number = self.c.get_purchases_number()
+        self.c.set_random_purchases_number(recurrency_key)
+        after_purchases_number = self.c.get_purchases_number()
+
+        self.assertEqual(previous_purchases_number, after_purchases_number)
 
 if __name__ == '__main__':
     unittest.main()
